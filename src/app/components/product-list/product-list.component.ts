d4d1b6e458b2f60178aaf355cd962337b5132563
Agbola Ayobami <me@ayoagbola.com.ng>
2025-02-05 02:01:09 +0100

    feat(product-card): add product images and expand product lists

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,10 +12,14 @@ import { CartService } from '../../services/cart/cart.service';
 })
 export class ProductListComponent {
   products: Product[] = [
-    { id: 1, name: 'Product 1', price: 10.00, imageUrl: 'assets/images/product.jpg' },
-    { id: 2, name: 'Product 2', price: 15.00, imageUrl: 'assets/images/product.jpg' },
-    { id: 3, name: 'Product 3', price: 20.00, imageUrl: 'assets/images/product.jpg' },
-    { id: 4, name: 'Product 4', price: 25.00, imageUrl: 'assets/images/product.jpg' }
+    { id: 1, name: 'XIAOMI Redmi Note 14', price: 130.43, imageUrl: 'assets/images/redmi_note_14.jpg' },
+    { id: 2, name: 'IPhone 15 Pro Max', price: 2000.20, imageUrl: 'assets/images/iphone15.jpg' },
+    { id: 3, name: 'Board Shoes', price: 15.78, imageUrl: 'assets/images/Board Shoes.jpg' },
+    { id: 4, name: 'Mens Casual Shoes', price: 6.42, imageUrl: 'assets/images/Casual Shoes.jpg' },
+    { id: 5, name: 'LED TV - Black', price: 105.23, imageUrl: 'assets/images/LED TV - Black.jpg' },
+    { id: 6, name: 'Buds 6 Play', price: 40.12, imageUrl: 'assets/images/Buds 6 Play.jpg' },
+    { id: 7, name: 'Wireless Mouse', price: 14.4, imageUrl: 'assets/images/Wireless Mouse.jpg' },
+    { id: 8, name: 'Hp ELITEBOOK X360', price: 23.40, imageUrl: 'assets/images/Hp ELITEBOOK X360.jpg' }
   ];
 
   constructor(private cartService: CartService) { }
